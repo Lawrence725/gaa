52badc6674ea5f27570bc29613982964374aba29
Lawrence <lawrence.0725@hotmail.com>
2022-03-08 22:24:59 +0800

    Add 電子發票, 優惠

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,39 @@
+// Popup (Discount)
+function popDiscount() {
+    var popDiscount = document.getElementById("popDiscount");
+    var close       = popDiscount.getElementsByClassName("close")[0];
+    var submit      = popDiscount.getElementsByClassName("submit")[0];
+    var cancel      = popDiscount.getElementsByClassName("cancel")[0];
+    close.onclick   = function() { popDiscount.style.display = "none"; }
+    submit.onclick  = function() { popDiscount.style.display = "none"; }
+    cancel.onclick  = function() { popDiscount.style.display = "none"; }
+    window.onclick  = function(event) { if (event.target == popDiscount) { popDiscount.style.display = "none"; }}
+    popDiscount.style.display = "flex"; }
+    
+// Popup (Invoice)
+function popInvoice() {
+    var popInvoice  = document.getElementById("popInvoice");
+    var close       = popInvoice.getElementsByClassName("close")[0];
+    var submit      = popInvoice.getElementsByClassName("submit")[0];
+    var cancel      = popInvoice.getElementsByClassName("cancel")[0];
+    close.onclick   = function() { popInvoice.style.display = "none"; }
+    submit.onclick  = function() { popInvoice.style.display = "none"; }
+    cancel.onclick  = function() { popInvoice.style.display = "none"; }
+    window.onclick  = function(event) { if (event.target == popInvoice) { popInvoice.style.display = "none"; }}
+    popInvoice.style.display = "flex"; }
+    
+// Popup (InvoiceDisable)
+function popInvoiceDisable() {
+    var popInvoiceDisable   = document.getElementById("popInvoiceDisable");
+    var close               = popInvoiceDisable.getElementsByClassName("close")[0];
+    var submit              = popInvoiceDisable.getElementsByClassName("submit")[0];
+    var cancel              = popInvoiceDisable.getElementsByClassName("cancel")[0];
+    close.onclick           = function() { popInvoiceDisable.style.display = "none"; }
+    submit.onclick          = function() { popInvoiceDisable.style.display = "none"; }
+    cancel.onclick          = function() { popInvoiceDisable.style.display = "none"; }
+    window.onclick          = function(event) { if (event.target == popInvoiceDisable) { popInvoiceDisable.style.display = "none"; }}
+    popInvoiceDisable.style.display = "flex"; }
+
 // Chart
 // https://www.chartjs.org/docs/latest/samples/bar/stacked-groups.html
 var c1 = '#F96332';
